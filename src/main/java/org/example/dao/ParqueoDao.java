fab659fa3f28f3e83023f5627ad1c04b1db0e181
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-14 20:05:39 -0600

    Se crea parqueoDao con sus metodos

--- src/main/java/org/example/dao/ParqueoDao.java
+++ src/main/java/org/example/dao/ParqueoDao.java
@@ -2,10 +2,17 @@ package org.example.dao;
 
 import org.example.model.ParqueoModel;
 
+import java.util.List;
+
 public interface ParqueoDao {
 
     void crear(ParqueoModel parqueoModel);
     ParqueoModel leer (int id);
+    void actualizar(ParqueoModel parqueoModel);
+    void eliminar(int id);
+    List<ParqueoModel> listaTodos();
+    
+
 
 
 
